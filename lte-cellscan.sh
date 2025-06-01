6446b6701373d9375700e6cffd2bd3beb92cf6ab
Raddyn <124372068+Raddyn@users.noreply.github.com>
2025-06-02 00:32:46 +0200

    Update lte-cellscan.sh

--- lte-cellscan.sh
+++ lte-cellscan.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHON_SCRIPT="$SCRIPT_DIR/cli/main.py" 
 
 usage() {
-  echo "Usage: $0 -f <frequency> [-o <open_file>] [-S <save_file>] [-s <sample_rate>] [-T <time>] [-d <debug>] [-n <num_of_scans>] [-N <FFT_size>]"
+  echo "Usage: $0 -f <frequency1,frequency2,...> [-o <open_file>] [-S <save_file>] [-s <sample_rate>] [-T <time>] [-d <debug>] [-n <num_of_scans>] [-N <FFT_size>]"
   exit 1
 }
 
@@ -15,13 +15,13 @@ num_of_scans="1"
 FFT_size="128"
 open_file=""
 save_file=""
-frequency=""
+frequencies=""
 
 while getopts ":o:S:f:s:T:dn:N:" opt; do
   case $opt in
   o) open_file="$OPTARG" ;;
   S) save_file="$OPTARG" ;;
-  f) frequency="$OPTARG" ;;
+  f) frequencies="$OPTARG" ;;
   s) sample_rate="$OPTARG" ;;
   T) capture_time="$OPTARG" ;;
   d) debug="--debug" ;;
@@ -37,17 +37,23 @@ if [ "$OPTIND" -eq 1 ]; then
   usage
 fi
 
-cmd="python3 \"$PYTHON_SCRIPT\""
-[ -n "$open_file" ] && cmd+=" -o \"$open_file\""
-[ -n "$frequency" ] && cmd+=" -f $frequency"
-[ -n "$save_file" ] && cmd+=" -S \"$save_file\""
-[ -n "$sample_rate" ] && cmd+=" -s $sample_rate"
-[ -n "$capture_time" ] && cmd+=" -T $capture_time"
-[ -n "$debug" ] && cmd+=" $debug"
-[ -n "$num_of_scans" ] && cmd+=" -n $num_of_scans"
-[ -n "$FFT_size" ] && cmd+=" -N $FFT_size"
+# Split frequencies into an array
+IFS=',' read -r -a frequency_array <<< "$frequencies"
 
-eval $cmd
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+# Iterate over each frequency and execute the command
+for frequency in "${frequency_array[@]}"; do
+  cmd="python3 \"$PYTHON_SCRIPT\""
+  [ -n "$open_file" ] && cmd+=" -o \"$open_file\""
+  [ -n "$frequency" ] && cmd+=" -f $frequency"
+  [ -n "$save_file" ] && cmd+=" -S \"$save_file\""
+  [ -n "$sample_rate" ] && cmd+=" -s $sample_rate"
+  [ -n "$capture_time" ] && cmd+=" -T $capture_time"
+  [ -n "$debug" ] && cmd+=" $debug"
+  [ -n "$num_of_scans" ] && cmd+=" -n $num_of_scans"
+  [ -n "$FFT_size" ] && cmd+=" -N $FFT_size"
+
+  eval $cmd
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+done
